55edc7ae51146156cbce5e195ac8d3c6d491ba09
gcastella <gcastella.91@gmail.com>
2015-11-06 12:07:27 +0100

    update of the options using .onLoad

--- R/statTools_code_OPTS.R
+++ R/statTools_code_OPTS.R
@@ -1,13 +1,15 @@
-options(parametric.tests = list(numeric.binary = list("t.test"), 
-                                numeric.multi = list("oneway.test", var.equal = TRUE),
-                                factor = list("chisq.test")))
-options(non.parametric.tests = list(numeric.binary = list("wilcox.test"), 
-                                numeric.multi = list("kruskal.test"),
-                                factor = list("fisher.test", simulate.p.value = TRUE)))
-options(p.value.rounding = list("pvalue"))
-options(condense.binary.factors = TRUE)
-options(rounding = 2)
-options(drop.x = TRUE)
-options(drop.y = TRUE)
-options(margin = 2)
-options(outcome = 1)
+.onLoad <- function(libname = find.package("statTools"), pkgname = "statTools") {
+  options(parametric.tests = list(numeric.binary = list("t.test"), 
+                                  numeric.multi = list("oneway.test", var.equal = TRUE),
+                                  factor = list("chisq.test")))
+  options(non.parametric.tests = list(numeric.binary = list("wilcox.test"), 
+                                  numeric.multi = list("kruskal.test"),
+                                  factor = list("fisher.test", simulate.p.value = TRUE)))
+  options(p.value.rounding = list("pvalue"))
+  options(condense.binary.factors = TRUE)
+  options(rounding = 2)
+  options(drop.x = TRUE)
+  options(drop.y = TRUE)
+  options(margin = 2)
+  options(outcome = 1)
+}
